a058043cb1a2f9a76ae443624803fb6d95ce6e9e
alexladnev <49565343+alexladnev@users.noreply.github.com>
2019-04-12 18:45:27 +0300

    server

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import *
+from galtons_table import get
+from different_chances import ggg
+from line import ggggg
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def goto():
+    return redirect("/galtons_table")
+
+@app.route("/galtons_table", methods=["GET", "POST"])
+def main():
+    if request.method == "POST":
+        number = request.form["stringi1"]
+        count = request.form["stringi2"]
+        result = get(int(number), int(count))
+        return render_template("result.html", result=result)
+    return render_template("dalton.html")
+
+@app.route("/different", methods=["GET", "POST"])
+def chance():
+    if request.method == "POST":
+        number = int(request.form["stringi1"])
+        count = int(request.form["stringi2"])
+        prob = list(map(float, request.form["stringi3"].split(", ")))
+        result = ggg(number, count, prob)
+        return render_template("result.html", result=result)
+    return render_template("pohui.html")
+
+
+@app.route("/line", methods=["GET", "POST"])
+def line():
+    if request.method == "POST":
+        number = int(request.form["stringi1"])
+        result = ", ".join(list(map(str, ggggg(number))))
+        return render_template("hui.html", result=result)
+    return render_template("line.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+
---
+++ galtons_table.py
@@ -0,0 +1,96 @@
+import math
+import random
+
+def get(n, k):
+    count = 0
+    az = 0
+    Q = [0] * (k + 1)
+    Z = [0] * (k + 1)
+    while count < 100:
+        i = 0
+        t = 0
+        j = 0
+        w = 0
+        d = 0
+        asa = 0
+        asas = 0
+        v = 1
+        p = 0
+        m = 1
+        A = [0] * (k + 1)
+        B = [0] * (k + 1)
+        C = [0] * (k + 1)
+        D = [0] * (k + 1)
+        F = [0] * (k + 1)
+        if n == 0:
+            m = 1
+
+        while i < n:
+            m = 2 * m
+            i = i + 1
+
+        while j < n + 1:
+            u = n - j
+            g = math.factorial(n) // math.factorial(j)
+            A[j] = g * 1000000 // math.factorial(u)
+            t = t + A[j]
+            j = j + 1
+
+        while p < n + 1:
+            B[p] = A[p] * 1000000 // t
+            p = p + 1
+
+        C[0] = 0
+
+        while v < n + 2:
+            C[v] = B[v - 1] + C[v - 1]
+            v = v + 1
+
+        while w < k:
+            F[w] = random.randint(0, 1000000)
+            if F[w] >= C[0] and F[w] < C[1]:
+                D[0] = D[0] + 1
+
+            elif F[w] >= C[1] and F[w] < C[2]:
+                D[1] = D[1] + 1
+
+            elif F[w] >= C[2] and F[w] < C[3]:
+                D[2] = D[2] + 1
+
+            elif F[w] >= C[3] and F[w] < C[4]:
+                D[3] = D[3] + 1
+
+            elif F[w] >= C[4] and F[w] < C[5]:
+                D[4] = D[4] + 1
+
+            elif F[w] >= C[5] and F[w] < C[6]:
+                D[5] = D[5] + 1
+
+            elif F[w] >= C[6] and F[w] < C[7]:
+                D[6] = D[6] + 1
+
+            elif F[w] >= C[7] and F[w] < C[8]:
+                D[7] = D[7] + 1
+
+            elif F[w] >= C[8] and F[w] < C[9]:
+                D[8] = D[8] + 1
+
+            elif F[w] >= C[9] and F[w] < C[10]:
+                D[9] = D[9] + 1
+
+            elif F[w] >= C[10] and F[w] < C[11]:
+                D[10] = D[10] + 1
+
+            w = w + 1
+
+        while asas < n + 1:
+            Q[asas] = Q[asas] + D[asas]
+            asas = asas + 1
+
+        count = count + 1
+    result = []
+    while az < n + 1:
+        Z[az] = Q[az] / 100
+        result.append(Z[az])
+        az = az + 1
+    return result
